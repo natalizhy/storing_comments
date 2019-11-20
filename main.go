03a8d3d39b4be43446415478fa480961ed90995b
Natali Zhylina <natalizhylina@Natalis-MacBook-Pro.local>
2019-11-20 18:03:20 +0200

    storing data

--- main.go
+++ main.go
@@ -3,15 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
 	"log"
 
-	"github.com/go.mongodb.org/mongo-driver/bson"
-	"github.com/go.mongodb.org/mongo-driver/mongo"
-	"github.com/go.mongodb.org/mongo-driver/mongo/options"
-	"github.com/go.mongodb.org/mongo-driver/mongo/readpref"
- 	"github.com/natalizhy/storing comments/assets/mongodb"
- 	//"github.com/mongodb/mongo-go-driver"
-
+	//"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	//"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 // You will be using this Trainer type later in the program
@@ -23,7 +21,8 @@ type Trainer struct {
 
 func main() {
 	// Rest of the code will go here
-	//CreateMongoClient()
+
+	// Create client
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
 	if err != nil {
 		log.Fatal(err)
@@ -42,10 +41,39 @@ func main() {
 	}
 
 	fmt.Println("Connected to MongoDB!")
-	collection := client.Database("test").Collection("trainers")
-	fmt.Println(collection)
 
-	err = client.Disconnect(context.TODO())
+	collection := client.Database("test").Collection("trainers")
+
+	_, err = collection.InsertOne(context.TODO(), Trainer{
+		Name: "vasy",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var results []*Trainer
+
+	filter := bson.M{}
+
+	cur, err := collection.Find(context.TODO(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for cur.Next(context.TODO()) {
+
+		// create a value into which the single document can be decoded
+		var elem Trainer
+		err := cur.Decode(&elem)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		results = append(results, &elem)
+
+		fmt.Println(elem.Name)
+	}
+
 
 	if err != nil {
 		log.Fatal(err)
